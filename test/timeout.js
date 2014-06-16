8c28ad63022bbe1d471734debb25e6ef925b8c82
Xiaopei Li <xiaopei.li@geneegroup.com>
2014-06-16 11:45:29 +0800

    增加对 "超时请求"测试 的注释

--- test/timeout.js
+++ test/timeout.js
@@ -3,10 +3,25 @@ var assert = require("assert");
 
 describe("JSON RPC on ZMQ later binding:", function(){
 
-    var path = 'ipc://zeromq-test3';
-    var server = new zRPC();
-    var client = new zRPC();
 
+    var file = 'zeromq-test-' + Math.random().toString(36).substring(7),
+        path = 'ipc://' + file;
+    process.on('exit', function() {
+        console.log('deleting '+ file);
+        require('fs').unlinkSync(file);
+    });
+    var server = new zRPC();
+    server.logger = {
+        debug: function(m) {
+            console.log('server', m);
+        }
+    };
+    var client = new zRPC();
+    client.logger = {
+        debug: function(m) {
+            console.log('client', m);
+        }
+    };
     client.connect(path);
 
     describe("Server API", function() {
@@ -19,7 +34,33 @@ describe("JSON RPC on ZMQ later binding:", function(){
             /** 设置 server 的 API */
             server.calling("foo", function(params, client_id) {
                 /** 此测试中, server 不应被 call */
-                done(new Error('server be called after this call already timeout'));
+                // done(new Error('server be called after this call already timeout'));
+
+
+                /**
+
+                 client 0MQ [hwh8999h0] => {"jsonrpc":"2.0","method":"foo","params":{"foo":"bar"},"id":"hwh8999h0"}
+                 client 0MQ [hwh8999h0] <= timeout
+
+                 // 如果 server 正常处理了已超时的请求, 则会返回结果
+
+                 server 0MQ [hwh8999h0] <= {"jsonrpc":"2.0","method":"foo","params":{"foo":"bar"},"id":"hwh8999h0"}
+                 server 0MQ [hwh8999h0] => AG7f4R3vik92rFDeEEWq2HE=: {"jsonrpc":"2.0","result":"bar","id":"hwh8999h0"}
+
+                 // 但返回的结果会被 client 解析为 Invalid Request 错误, 并将此错误汇报回 server
+
+                 client 0MQ [hwh8999h0] <= {"jsonrpc":"2.0","result":"bar","id":"hwh8999h0"}
+                 client 0MQ [undefined] => {"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid Request"}}
+
+                 // 而 server 收到该错误后, 也会解析为 Invalid Request, 并再次踢回给 client
+
+                 server 0MQ [N/A] <= {"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid Request"}}
+                 server 0MQ [undefined] => AG7f4R3vik92rFDeEEWq2HE=: {"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid Request"}}
+                 client 0MQ [N/A] <= {"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid Request"}}
+
+                 // 会如此反反复复无穷尽也
+
+                 */
                 return params.foo;
             });
 
@@ -53,3 +94,4 @@ describe("JSON RPC on ZMQ later binding:", function(){
     });
 
 });
+
