b730d4d3a57b9a71a9a7333527d97810900b008c
Xiaopei Li <xiaopei.li@geneegroup.com>
2014-06-17 17:40:32 +0800

    完善超时测试

--- test/timeout.js
+++ test/timeout.js
@@ -1,32 +1,44 @@
-var zRPC = require('../');
-var assert = require("assert");
+var zRPC = require('../'),
+    assert = require("assert"),
+    fs = require('fs'),
+    moment = require('moment');
 
 describe("JSON RPC on ZMQ later binding:", function(){
 
-    var file = 'zeromq-test-' + Math.random().toString(36).substring(7),
-        path = 'ipc://' + file;
-
-    process.on('exit', function() {
-        console.log('deleting '+ file);
-        require('fs').unlinkSync(file);
-    });
-
-    var server = new zRPC();
-    server.logger = {
-        debug: function(m) {
-            console.log('server', m);
-        }
-    };
-
-    var client = new zRPC();
-    client.logger = {
-        debug: function(m) {
-            console.log('client', m);
-        }
-    };
-    client.connect(path);
+    var file,
+        path,
+        server,
+        client,
+        begin;
 
     describe("Server API", function() {
+
+        beforeEach(function() {
+            begin = moment().valueOf();
+            file = 'zeromq-test-' + Math.random().toString(36).substring(7);
+            path = 'ipc://' + file;
+
+            server = new zRPC();
+            server.logger = {
+                debug: function(m) {
+                    console.log(moment().valueOf() - begin, 'server', m);
+                }
+            };
+
+            client = new zRPC();
+            client.logger = {
+                debug: function(m) {
+                    console.log(moment().valueOf() - begin, 'client', m);
+                }
+            };
+            client.connect(path);
+        });
+
+        afterEach(function() {
+            server = null;
+            client = null;
+            fs.unlinkSync(file);
+        });
 
         it("should not be called after client call timeout", function(done) {
 
@@ -36,38 +48,18 @@ describe("JSON RPC on ZMQ later binding:", function(){
             /** 设置 server 的 API */
             server.calling("foo", function(params, client_id) {
 
-                /**
-
-                 client 0MQ [hwh8999h0] => {"jsonrpc":"2.0","method":"foo","params":{"foo":"bar"},"id":"hwh8999h0"}
-                 client 0MQ [hwh8999h0] <= timeout
-
-                 // 如果 server 正常处理了已超时的请求, 则会返回结果
-
-                 server 0MQ [hwh8999h0] <= {"jsonrpc":"2.0","method":"foo","params":{"foo":"bar"},"id":"hwh8999h0"}
-                 server 0MQ [hwh8999h0] => AG7f4R3vik92rFDeEEWq2HE=: {"jsonrpc":"2.0","result":"bar","id":"hwh8999h0"}
-
-                 // 但返回的结果会被 client 解析为 Invalid Request 错误, 并将此错误汇报回 server
-
-                 client 0MQ [hwh8999h0] <= {"jsonrpc":"2.0","result":"bar","id":"hwh8999h0"}
-                 client 0MQ [undefined] => {"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid Request"}}
-
-                 // 而 server 收到该错误后, 也会解析为 Invalid Request, 并再次踢回给 client
-
-                 server 0MQ [N/A] <= {"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid Request"}}
-                 server 0MQ [undefined] => AG7f4R3vik92rFDeEEWq2HE=: {"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid Request"}}
-                 client 0MQ [N/A] <= {"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid Request"}}
-
-                 // 会如此反反复复无穷尽也
-
-                 */
-
                 /** 此测试中, server 不应被 call */
                 done(new Error('server be called after this call already timeout'));
 
                 return params.foo;
             });
 
-            /** 设置 client call timeout 时长 */
+            /** 设置 server 很晚 (在 client call timeout 后) 才 bind */
+            setTimeout(function() {
+                server.bind(path);
+            }, 2000);
+
+            /** 设置 client callTimeout */
             client.callTimeout = 1000;
 
             /** client call */
@@ -75,22 +67,63 @@ describe("JSON RPC on ZMQ later binding:", function(){
             .call("foo", {foo:"bar"})
             .done(function(ret){
                 /* 不应到此 */
-                done(new Error('client call should not be done without server'));
+                done(new Error('client call should not be done'));
             }, function(err) {
-                /** 应该到此 */
+                /** 1s 后应该到此 */
                 assert.equal(err.code, -32603);
                 assert.equal(err.message, 'Call Timeout');
             });
 
-            /** 在 client call timeout 后, server 才 bind */
+            /** server bind 后, 测试才结束 */
+            setTimeout(function() {
+                done();
+            }, 3000);
+
+        });
+
+        /**
+         * 如果 server 设置了较短的 timeout, 则即使 client 设置的
+         * call timeout 较长, 也按 server 的设置判断超时
+         */
+        it("should not be called after server call timeout", function(done) {
+
+            /** 设置测试时长较长 */
+            this.timeout(10000);
+
+            /** 设置 server 的 API */
+            server.serverTimeout = 1000;
+            server.calling("foo", function(params, client_id) {
+
+                /** 此测试中, server 不应被 call */
+                done(new Error('server be called after this call already timeout'));
+
+                return params.foo;
+            });
+
+            /** 设置 server 很晚 (在 client call timeout 后) 才 bind */
             setTimeout(function() {
                 server.bind(path);
-            }, 2000);
+            }, 1500);
+
+            /** 设置 client callTimeout */
+            client.callTimeout = 2000;
+
+            /** client call */
+            client
+            .call("foo", {foo:"bar"})
+            .done(function(ret){
+                /* 不应到此 */
+                done(new Error('client call should not be done'));
+            }, function(err) {
+                /** 1s 后应该到此 */
+                assert.equal(err.code, -32603);
+                assert.equal(err.message, 'Call Timeout');
+            });
 
             /** server bind 后, 测试才结束 */
             setTimeout(function() {
                 done();
-            }, 5000);
+            }, 3000);
 
         });
 
